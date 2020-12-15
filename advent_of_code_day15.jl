0fcb4a5a3d7444f6545400cf06ec35a4738677a2
Jordan Jameson <njordan.jameson@gmail.com>
2020-12-15 10:03:53 -0500

    Add files via upload

---
+++ advent_of_code_day15.jl
@@ -0,0 +1,34 @@
+x = split("5,1,9,18,13,8,0", ",")
+
+function make_dict(x)
+    D = Dict{Int, Array{Int}}(n => [ii,1] for (ii,n) in enumerate(parse.(Int, x[1:end-1])))
+    return D
+end
+
+function both_parts(D, input, limit)
+    D = copy(D)
+    for ii in (length(D)+1):(limit -1)
+        if haskey(D, input)
+            D[input][2] += 1
+            D[input][1],input = ii, (ii - D[input][1])
+        else
+            D[input] = [ii,1]
+            input = 0
+        end
+    end
+    return input
+end
+
+
+## part 1
+A = make_dict(x)
+start = parse(Int, x[end])
+answer1 = both_parts(A, start, 2020)
+
+
+## part 2
+A = make_dict(x)
+start = parse(Int, x[end])
+@time begin
+answer2 = both_parts(A, start, 30000000)
+end
